cd95dd5c166096b010cd3d7e283378338eda10a9
Yash <ysolanki069@gmail.com>
2022-09-05 18:26:54 +0530

    getDeaultProvider update and example.config.json goerli&rinkeby code changes to make it work

--- utils/getProvider.js
+++ utils/getProvider.js
@@ -2,8 +2,21 @@ const ethers = require("ethers");
 const { networks } = require("../config.json");
 
 module.exports = (networkName) => {
-  let url =
-    networks[networkName].INFURA_URL ?? networks[networkName].ALCHEMY_URL;
+  if (networkName == "mumbai") {
+    let url =
+      networks[networkName].INFURA_URL ?? networks[networkName].ALCHEMY_URL;
+    return new ethers.providers.JsonRpcProvider(url);
+  }
 
-  return new ethers.providers.JsonRpcProvider(url);
+  const network = networks[networkName];
+  const provider = ethers.getDefaultProvider(network, {
+    infura: networks[networkName].INFURA_KEY,
+    // Or if using a project secret:
+    // infura: {
+    //   projectId: YOUR_INFURA_PROJECT_ID,
+    //   projectSecret: YOUR_INFURA_PROJECT_SECRET,
+    // },
+    alchemy: networks[networkName].ALCHEMY_URL,
+  });
+  return provider;
 };
