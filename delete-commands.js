3636a53668a0e646f3e0ace7dd99853470277cef
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-11-14 15:13:59 +0530

    Fixed up repo

--- delete-commands.js
+++
@@ -1,6 +0,0 @@
-//! Run `node delete-commands.js` to delete all the commands
-
-const deleteCommands = require("./utils/deleteCommands");
-
-// Pass true to remove all commands globally (i.e : deleteCommands(true))
-deleteCommands();
--- utils/getTxName.js
+++
@@ -1,8 +0,0 @@
-// Used in Faucet_response.js to display the transaction hash link
-
-module.exports = async (networkName) => {
-  if (networkName == "mumbai") {
-    return `https://${networkName}.polygonscan.com/tx/`;
-  }
-  return `https://${networkName}.etherscan.io/tx/`;
-};
