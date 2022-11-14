2fce101cdddd2c6873aeb2b7d7ba1b0e362f54b6
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-11-14 09:17:28 +0530

    Changed Config Structure and worked on dynamic options

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -1,12 +1,12 @@
 /* 
-Get the Faucet Address balance of the Passed Network and token. If the token is not passed then the default native-currency is used
-ADMINS ONLY
-* Change this if you add more networks/tokens and deploy the commands again using `node deploy-commands`
+* Get the Faucet Address balance of the Passed Network and token. If the token is not passed then the default native-currency is used
+! ADMINS ONLY
+If you change this, make sure to run `pnpm bot:deletecommands && pnpm bot:addcommands`
 */
 
 import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 
-// TODO : Make this object dynamic (Get from config)
+import { networks, tokens } from "../config/config.json";
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -16,34 +16,28 @@ module.exports = {
 		)
 		.setDMPermission(false)
 		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-		.addStringOption(option =>
-			option
-				.setName("network")
-				.setDescription("Select the network")
-				.setRequired(true)
-				.addChoices(
-					{
-						name: "Goerli",
-						value: "goerli",
-					},
-					{
-						name: "Rinkeby",
-						value: "rinkeby",
-					},
-					{
-						name: "Mumbai",
-						value: "mumbai",
-					}
-				)
-		)
-		.addStringOption(option =>
+		.addStringOption(option => {
+			option.setName("network").setDescription("Select the network").setRequired(true);
+
+			networks.forEach(network => {
+				option.addChoices({
+					name: `${network.name.toUpperCase()}`,
+					value: `${network.name.toLowerCase()}`,
+				});
+			});
+			return option;
+		})
+		.addStringOption(option => {
 			option
 				.setName("token")
 				.setDescription("Select the token if applicable")
-				.setRequired(false)
-				.addChoices({
-					name: "LINK",
-					value: "link",
-				})
-		),
+				.setRequired(false);
+			tokens.forEach(token => {
+				option.addChoices({
+					name: `${token.name.toUpperCase()}`,
+					value: `${token.name.toLowerCase()}`,
+				});
+			});
+			return option;
+		}),
 };
